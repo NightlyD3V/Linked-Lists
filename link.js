24ba768b06d2ad823257a564f3bc1260096a065e
Tyler Spaulding <45692071+NightlyD3V@users.noreply.github.com>
2023-07-27 14:03:26 -0400

    Update link.js

--- link.js
+++ link.js
@@ -1,7 +1,20 @@
-//ES6 functions support constructors?
-const linkedlist = (node, position) => {
-    constructor(node, position) {
-      this.node = null
-      this.positon = 0
+//ES6 functions support constructors? 
+const linkedlist = (node, next) => {
+    this.node = null
+    this.next = 0
+
+    const list = {
+        head: {
+            value: 0,
+            next: {
+                value: 1,
+                next: {
+                    value: 2,
+                    next: {
+                        value: null
+                    }
+                }
+            }
+        }
     }
 }
