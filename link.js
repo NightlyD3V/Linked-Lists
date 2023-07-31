54848b86e9af56d91deda1feb552633f8350d07c
ghost <ghost@pop-os.localdomain>
2023-07-31 18:09:24 -0400

    Implemented a simple linked list

--- link.js
+++ link.js
@@ -1,20 +1,21 @@
 //ES6 functions support constructors? 
-const linkedlist = (node, next) => {
-    this.node = null
-    this.next = 0
-
-    const list = {
-        head: {
-            value: 0,
-            next: {
-                value: 1,
-                next: {
-                    value: 2,
-                    next: {
-                        value: null
-                    }
-                }
-            }
-        }
+class ListNode {
+    constructor(node) {
+        this.node = node
+        this.next = null
     }
 }
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head 
+    }
+}
+
+let node1 = new ListNode(2)
+let node2 = new ListNode(5)
+node1.next = node2
+
+let myList = new LinkedList(node1)
+
+console.log(myList.head.next.node)
